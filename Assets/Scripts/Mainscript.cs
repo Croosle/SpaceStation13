f2c6b70821440e7171a40288bb79a918b31f18ba
Croosle <iartifical@gmail.com>
2015-08-04 12:54:17 +0700

    Added floor

---
+++ Assets/Scripts/Mainscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mainscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionExit(Collision collisionInfo) {
+		print("No longer in contact with " + collisionInfo.transform.name);
+	}
+}
