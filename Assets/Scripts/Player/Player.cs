b6552993c8c4ef79fe6e39f867f0b306890d8c0b
atme <d3ljke@gmail.com>
2016-03-19 14:15:57 +0100

    network is enabled

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    public new Camera camera;
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            camera.enabled = false;
+            camera.GetComponent<AudioListener>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "usable" && Input.GetKeyDown(KeyCode.F))
+            {
+                GameObject usableObject = hit.collider.gameObject;
+                Action action = (Action)usableObject.GetComponent(typeof(Action));
+                action.Use();
+            }
+        }
+    }
+}
